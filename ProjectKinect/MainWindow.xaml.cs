85e487e945c7b8b3bee7e0d4944e83fc763b95aa
송민호 <smh0660@gmail.com>
2017-07-14 09:40:53 +0900

    이미지불러오기 커밋

--- ProjectKinect/MainWindow.xaml.cs
+++ ProjectKinect/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using ProjectKinect.Model;
+﻿using MySql.Data.MySqlClient;
+using ProjectKinect.Model;
 using ProjectKinect.Weather;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,6 +14,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -19,20 +23,50 @@ using System.Windows.Threading;
 
 namespace ProjectKinect
 {
+    
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
     {
+        static DependencyProperty dp =
+        DependencyProperty.Register("setImage", typeof(System.Drawing.Image), typeof(System.Windows.Controls.Image), null);
 
+        static MySqlConnection connect = null;
+        static MySqlCommand sqlcmd = null;
+        static MySqlDataReader sqlread = null;
+
+        static System.Drawing.Image imageFile = null;
+
+        static FileStream fstream = null;
+        static BinaryReader binread = null;
+        static MemoryStream mstream = null;
+    
         public MainWindow()
         {
-            Database database = new Database();
-            database.test();
+            
+            InitializeComponent();
+
+            //string strConn =  "Server=localhost; Database=styler; Uid=root; Pwd=c00k!erun;";
+            string strConn = "Server=122.44.13.91; Port = 11059; Database=styler; Uid=root;Pwd=1;";
+            // Image Insert Ex.
+
+            try
+            {
+                connect = new MySqlConnection(strConn);
+
+                connect.Open();
+
+                Console.WriteLine("DB와 연결 성공");
+            }
+            catch
+            {
+                Console.WriteLine("DB와 연결 안됨 Error");
+            }
+
             Get_Weather();
             InitTimer();
-
-            InitializeComponent();
+            test2();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -103,6 +137,61 @@ namespace ProjectKinect
         {
             ClothesSelect CS = new ClothesSelect();
             CS.ShowDialog();
+        }
+
+        public ImageSource ImageSourceForBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+
+            return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        public void test2()
+        {
+            try
+            {
+                string query = "select image from gallery where galleryid = 3";
+
+                sqlcmd = new MySqlCommand(query, connect);
+
+                sqlread = sqlcmd.ExecuteReader();
+
+                byte[] imageData = null;
+
+                try
+                {
+                    while (sqlread.Read())
+                    {
+                        imageData = (byte[])sqlread[0];
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("변환 중 오류 발생");
+                }
+
+                sqlread.Close();
+
+                mstream = new MemoryStream(imageData);
+
+                imageFile = System.Drawing.Image.FromStream(mstream);  // 이 시점인가?!
+
+                Console.WriteLine(System.Drawing.Image.FromStream(mstream).GetType());
+                Console.WriteLine("성공적으로 변환했습니다!");
+
+                Bitmap bitmapFile = (Bitmap)imageFile;
+                var handle = bitmapFile.GetHbitmap();
+
+                //이미지 삽입
+
+                Img.Source =
+                    Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch
+            {
+                Console.WriteLine("실패");
+            }
+
         }
     }
 }
